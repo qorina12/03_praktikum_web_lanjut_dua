0962ad20427baa9c6632799e83b7aef0fc4af438
qorina12 <prameswarides12@gmail.com>
2021-03-02 23:47:06 +0700

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    //
+    public function index() {
+        return view('index');
+    }
+
+    public function post() {
+        return view('post');
+    }
+
+    public function about() {
+        return view('about');
+    }
+
+    public function contact() {
+        return view('contact');
+    }
+}
